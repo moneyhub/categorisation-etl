a400e4ea09d0f02641bd2fb962464b52228daf04
Omar Zamarripa <omar.zamarripa@moneyhub.com>
2024-05-20 12:32:05 +0300

    Add placeholder for client config

--- config/client.js
+++ config/client.js
@@ -3,6 +3,13 @@ export default {
   identityServiceUrl: "https://identity.moneyhub.co.uk",
   accountConnectUrl: "https://bank-chooser.moneyhub.co.uk/account-connect.js",
   client: {
-
+    client_id: "CLIENT_ID",
+    client_secret: "CLIENT_SECRET",
+    token_endpoint_auth_method: "client_secret_basic",
+    id_token_signed_response_alg: "RS256",
+    request_object_signing_alg: "none",
+    redirect_uri: "REDIRECT_URI",
+    response_type: "code",
+    keys: [/* your jwks */],
   },
 }
